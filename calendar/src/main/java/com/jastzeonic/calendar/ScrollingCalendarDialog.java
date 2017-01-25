6965211774ff12f1fc8c8ab0751e73b3d24bb4e3
Jast Lai by Mac <jast.lai@ptc-nec.com.tw>
2017-01-25 15:43:41 +0800

    * fix some bug

--- calendar/src/main/java/com/jastzeonic/calendar/ScrollingCalendarDialog.java
+++ calendar/src/main/java/com/jastzeonic/calendar/ScrollingCalendarDialog.java
@@ -28,11 +28,13 @@ public class ScrollingCalendarDialog {
     private Context context;
     private Dialog dialog;
 
+    private TextView mTextViewYear;
+    private TextView mTextViewMonth;
+    RecyclerView mRecyclerView;
 
     public ScrollingCalendarDialog(Context context) {
         this.context = context;
-        dialog = new Dialog(context, R.style.NewDialog);
-        dialog.setContentView(R.layout.dialog_calendar);
+
 
     }
 
@@ -40,105 +42,111 @@ public class ScrollingCalendarDialog {
 
     public void callCalendarView(TextView startDate, final TextView setTextView) {
 
-        final RecyclerView mRecyclerView = (RecyclerView) dialog.findViewById(R.id.recycler_view);
-        final TextView mTextViewYear = (TextView) dialog.findViewById(R.id.calendar_date_year_display);
-        final TextView mTextViewMonth = (TextView) dialog.findViewById(R.id.calendar_date_month_display);
 
-        LinearSnapHelper helper = new LinearSnapHelper() {
-            @Override
-            public int findTargetSnapPosition(RecyclerView.LayoutManager layoutManager, int velocityX, int velocityY) {
-                View centerView = findSnapView(layoutManager);
-                if (centerView == null)
-                    return RecyclerView.NO_POSITION;
+        if (dialog == null) {
+            dialog = new Dialog(context, R.style.NewDialog);
+            dialog.setContentView(R.layout.dialog_calendar);
+            mRecyclerView = (RecyclerView) dialog.findViewById(R.id.recycler_view);
+            mTextViewYear = (TextView) dialog.findViewById(R.id.calendar_date_year_display);
+            mTextViewMonth = (TextView) dialog.findViewById(R.id.calendar_date_month_display);
+
+            LinearSnapHelper helper = new LinearSnapHelper() {
+                @Override
+                public int findTargetSnapPosition(RecyclerView.LayoutManager layoutManager, int velocityX, int velocityY) {
+                    View centerView = findSnapView(layoutManager);
+                    if (centerView == null)
+                        return RecyclerView.NO_POSITION;
 
 
-                int position = layoutManager.getPosition(centerView);
-                Log.v("ScrollingCalendarDialog", "centerView.getId():" + position);
-                int targetPosition = -1;
-                if (layoutManager.canScrollHorizontally()) {
-                    Log.v("ScrollingCalendarDialog", "velocityX:" + velocityX);
-                    if (velocityX < 0) {
-                        targetPosition = position - 1;
-                    } else {
-                        targetPosition = position + 1;
+                    int position = layoutManager.getPosition(centerView);
+                    Log.v("ScrollingCalendarDialog", "centerView.getId():" + position);
+                    int targetPosition = -1;
+                    if (layoutManager.canScrollHorizontally()) {
+                        Log.v("ScrollingCalendarDialog", "velocityX:" + velocityX);
+                        if (velocityX < 0) {
+                            targetPosition = position - 1;
+                        } else {
+                            targetPosition = position + 1;
+                        }
                     }
+
+                    if (layoutManager.canScrollVertically()) {
+                        if (velocityY < 0) {
+                            targetPosition = position - 1;
+                        } else {
+                            targetPosition = position + 1;
+                        }
+                    }
+
+                    final int firstItem = 0;
+                    final int lastItem = layoutManager.getItemCount() - 1;
+                    targetPosition = Math.min(lastItem, Math.max(targetPosition, firstItem));
+
+
+                    currentPosition = targetPosition;
+                    return targetPosition;
                 }
 
-                if (layoutManager.canScrollVertically()) {
-                    if (velocityY < 0) {
-                        targetPosition = position - 1;
-                    } else {
-                        targetPosition = position + 1;
-                    }
+            };
+            helper.attachToRecyclerView(mRecyclerView);
+
+
+            mRecyclerView.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false));
+
+
+            List<Calendar> items = new ArrayList<>();
+            for (int i = 0; i < 3; i++) {
+
+                Calendar calendar = Calendar.getInstance();
+                calendar.add(Calendar.MONTH, i);
+                items.add(calendar);
+            }
+            mRecyclerView.setAdapter(new RecyclerViewAdapter(items, new CalendarEventHandler() {
+                @Override
+                public void onDayPress(Calendar date) {
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd", Locale.getDefault());
+
+                    String selectTime = sdf.format(date.getTime());
+                    setTextView.setText(selectTime);
+                    dialog.dismiss();
                 }
+            }));
+            mRecyclerView.setNestedScrollingEnabled(false);
 
-                final int firstItem = 0;
-                final int lastItem = layoutManager.getItemCount() - 1;
-                targetPosition = Math.min(lastItem, Math.max(targetPosition, firstItem));
+            mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+                @Override
+                public void onScrollStateChanged(final RecyclerView recyclerView, int scrollState) {
 
+                    switch (scrollState) {
 
-                currentPosition = targetPosition;
-                return targetPosition;
-            }
+                        case RecyclerView.SCROLL_STATE_SETTLING:
 
-        };
-        helper.attachToRecyclerView(mRecyclerView);
+                            mRecyclerView.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    ViewHolder mViewHolder = ((ViewHolder) mRecyclerView.findViewHolderForAdapterPosition(currentPosition));
+                                    if (mViewHolder != null) {
+                                        Calendar currentDate = mViewHolder.mCalendarView.getCurrentDate();
+                                        SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_YEAR, Locale.getDefault());
+                                        SimpleDateFormat sdf2 = new SimpleDateFormat(DATE_FORMAT_MONTH, Locale.getDefault());
 
-
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false));
-
-
-        List<Calendar> items = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-
-            Calendar calendar = Calendar.getInstance();
-            calendar.add(Calendar.MONTH, i);
-            items.add(calendar);
-        }
-        mRecyclerView.setAdapter(new RecyclerViewAdapter(items, new CalendarEventHandler() {
-            @Override
-            public void onDayPress(Calendar date) {
-                SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd", Locale.getDefault());
-
-                String selectTime = sdf.format(date.getTime());
-                setTextView.setText(selectTime);
-                dialog.dismiss();
-            }
-        }));
-        mRecyclerView.setNestedScrollingEnabled(false);
-
-        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrollStateChanged(final RecyclerView recyclerView, int scrollState) {
-
-                switch (scrollState) {
-
-                    case RecyclerView.SCROLL_STATE_SETTLING:
-
-                        mRecyclerView.postDelayed(new Runnable() {
-                            @Override
-                            public void run() {
-                                ViewHolder mViewHolder = ((ViewHolder) mRecyclerView.findViewHolderForAdapterPosition(currentPosition));
-                                if (mViewHolder != null) {
-                                    Calendar currentDate = mViewHolder.mCalendarView.getCurrentDate();
-                                    SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_YEAR, Locale.getDefault());
-                                    SimpleDateFormat sdf2 = new SimpleDateFormat(DATE_FORMAT_MONTH, Locale.getDefault());
-
-                                    mTextViewYear.setText(sdf.format(currentDate.getTime()));
-                                    mTextViewMonth.setText((sdf2.format(currentDate.getTime())));
+                                        mTextViewYear.setText(sdf.format(currentDate.getTime()));
+                                        mTextViewMonth.setText((sdf2.format(currentDate.getTime())));
+                                    }
                                 }
-                            }
-                        }, 100);
+                            }, 100);
 
 
-                        break;
+                            break;
+
+                    }
+
 
                 }
 
+            });
 
-            }
-
-        });
+        }
 
         Calendar currentDate = Calendar.getInstance();
 
@@ -147,8 +155,7 @@ public class ScrollingCalendarDialog {
 
         mTextViewYear.setText(sdf.format(currentDate.getTime()));
         mTextViewMonth.setText((sdf2.format(currentDate.getTime())));
-
-
+        
         dialog.show();
 
     }
